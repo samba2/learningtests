2106a0da827f83da34b5e01ab98576a946584954
Maik Toepfer <maik.toepfer@prisma-capacity.eu>
2020-11-06 13:55:20 +0100

    more tests

--- mapstruct/src/test/java/org/samba/MapstructLearningTest.java
+++ mapstruct/src/test/java/org/samba/MapstructLearningTest.java
@@ -6,7 +6,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
+
+import java.time.Month;
+import java.time.ZonedDateTime;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,19 +21,20 @@ public class MapstructLearningTest {
 
     // POJOs had to be static
     @Data
-//    @AllArgsConstructor
-//    @NoArgsConstructor
-    public static class SimpleSource {
+    @AllArgsConstructor
+    static class SimpleSource {
         private String name;
         private String description;
     }
 
     @Data
-    public static class SimpleDestination {
+    @AllArgsConstructor
+    static class SimpleDestination {
         private String name;
         private String description;
     }
 
+    // to register DI in spring: @Mapper(componentModel = "spring")
     @Mapper
     public interface SimpleSourceDestinationMapper {
         SimpleDestination sourceToDestination(SimpleSource source);
@@ -35,17 +42,69 @@ public class MapstructLearningTest {
     }
 
     @Test
-    public void simpleMap() {
+    public void simpleMapSourceToDestination() {
         SimpleSourceDestinationMapper mapper =
                 Mappers.getMapper(SimpleSourceDestinationMapper.class);
 
-        SimpleSource source = new SimpleSource();
-        source.setName("Ronny");
-        source.setDescription("mate");
+        SimpleSource source = new SimpleSource("Ronny", "mate");
         SimpleDestination destination = mapper.sourceToDestination(source);
 
         assertEquals(source.getName(), destination.getName());
         assertEquals(source.getDescription(), destination.getDescription());
     }
+
+    @Test
+    public void destinationToSource() {
+        SimpleSourceDestinationMapper mapper =
+                Mappers.getMapper(SimpleSourceDestinationMapper.class);
+
+        SimpleDestination destination = new SimpleDestination("Ronny", "mate");
+        SimpleSource source = mapper.destinationToSource(destination);
+
+        assertEquals(destination.getName(), source.getName());
+        assertEquals(destination.getDescription(), source.getDescription());
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class VinylRecord {
+        private Date releaseYear;
+        private String artist;
+        private String title;
+    }
+
+    @Data
+    static class ShopProduct {
+        private ZonedDateTime manufactured;
+        private String mainTitle;
+        private String description;
+    }
+
+
+    @Mapper
+    public interface VinylRecordMapper {
+        @Mappings({
+                @Mapping(target = "mainTitle", source = "vinylRecord.artist"),
+                @Mapping(target = "description", source = "vinylRecord.title"),
+                @Mapping(target = "manufactured", source = "vinylRecord.releaseYear")
+        })
+        ShopProduct vinylRecordToShopProduct(VinylRecord vinylRecord);
+        // other conversion direction missing due to laziness
+    }
+
+    @Test
+    public void vinylRecordToShopProduct() {
+        var mapper = Mappers.getMapper(VinylRecordMapper.class);
+
+        VinylRecord vinylRecord = new VinylRecord(new Date(2017 - 1900, 8, 1), "Great Artist", "Best Of");
+        ShopProduct shopProduct = mapper.vinylRecordToShopProduct(vinylRecord);
+
+        assertEquals(vinylRecord.getArtist(), shopProduct.getMainTitle());
+        assertEquals(vinylRecord.getTitle(), shopProduct.getDescription());
+        assertEquals(2017, shopProduct.getManufactured().getYear());
+        assertEquals(Month.SEPTEMBER, shopProduct.getManufactured().getMonth());
+        assertEquals(1, shopProduct.getManufactured().getDayOfMonth());
+    }
+
 
 }
