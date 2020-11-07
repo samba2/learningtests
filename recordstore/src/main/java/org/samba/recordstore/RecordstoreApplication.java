1c9291322063d834a64a8e4b8058396088db01e4
Maik Toepfer <maik.toepfer@prisma-capacity.eu>
2020-11-07 23:45:36 +0100

    towards integration test

---
+++ recordstore/src/main/java/org/samba/recordstore/RecordstoreApplication.java
@@ -0,0 +1,11 @@
+package org.samba.recordstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordstoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RecordstoreApplication.class, args);
+    }
+}
---
+++ recordstore/src/test/java/org/samba/recordstore/IntegrationTest.java
@@ -0,0 +1,21 @@
+package org.samba.recordstore;
+
+import org.factcast.test.AbstractFactCastIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+@ActiveProfiles({"test", "integration"})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public class IntegrationTest extends AbstractFactCastIntegrationTest {
+
+    // TODO
+    @Test
+    public void makeMeRun() {
+
+    }
+}
