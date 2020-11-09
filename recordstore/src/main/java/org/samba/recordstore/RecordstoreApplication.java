f42da5547885485397c46248c8877c5e0e203566
Maik Toepfer <maik.toepfer@bmw.de>
2020-11-09 23:24:02 +0100

    build json manually

--- recordstore/src/main/java/org/samba/recordstore/RecordstoreApplication.java
+++ recordstore/src/main/java/org/samba/recordstore/RecordstoreApplication.java
@@ -3,6 +3,7 @@ package org.samba.recordstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// TODO come up with local setup
 @SpringBootApplication
 public class RecordstoreApplication {
     public static void main(String[] args) {
--- recordstore/src/test/java/org/samba/recordstore/IntegrationTest.java
+++ recordstore/src/test/java/org/samba/recordstore/IntegrationTest.java
@@ -1,11 +1,21 @@
 package org.samba.recordstore;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.factcast.test.AbstractFactCastIntegrationTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -13,9 +23,27 @@ import org.springframework.test.context.ActiveProfiles;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class IntegrationTest extends AbstractFactCastIntegrationTest {
 
-    // TODO add integration tests
-    @Test
-    public void makeMeRun() {
+    @Autowired
+    private MockMvc mockMvc;
 
+    // TODO fails with: org.springframework.web.util.NestedServletException: Request processing failed; nested exception is org.springframework.http.converter.HttpMessageConversionException: Type definition error: [simple type, class java.time.LocalDate]; nested exception is com.fasterxml.jackson.databind.exc.InvalidDefinitionException: Cannot construct instance of `java.time.LocalDate` (no Creators, like default constructor, exist): no String-argument constructor/factory method to deserialize from String value ('2020-01-30')
+    // seems like a config/ missing dependency problem ?!
+    @Test
+    public void makeMeRun() throws Exception {
+        var mapper = new ObjectMapper();
+        var newRecord = mapper.createObjectNode();
+        newRecord.put("artist", "The Dancing Monkeys");
+        newRecord.put("title", "Dancing Time");
+        newRecord.put("label", "Ape Records");
+        newRecord.put("format", "12");
+        newRecord.put("releaseDate", "2020-01-30");
+
+        var json = mapper.writeValueAsString(newRecord);
+
+        this.mockMvc.perform(post("/recordstore")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andDo(print());
+//        assertTrue(controller != null);
     }
 }
